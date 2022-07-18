5dc25cac16c8a9c5156fca84bdd7386f0b948ea2
Susnik20 <anikh725@gmail.com>
2022-07-18 15:12:25 +0530

    1st api completed

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,21 +1,43 @@
-const urlModel =require('../model/urlModel')
-const validUrl = require('valid-url');
+const urlModel = require('../model/urlModel')
+
 const shortid = require('shortid');
 
+let isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if(typeof value === String && value.trim().length === 0) return true
+}
 
 
-const urlShortner= async function(req, res){
+const urlShortner = async function (req, res) {
     try {
-        let data= req.body
-        let {urlCode, longUrl, shortUrl,...rest}=data
-        const createNewUrl =await urlModel.create(data);
-        res.status(400).send({status:true, data:createNewUrl})
-    } catch (error) {res.status(500).send({status:false, message: error.message})
+        let data = req.body
+        let { longUrl, ...rest } = data
+        if(Object.keys(data).length === 0) return res.status(400).send({status:false,message:"Req.body is empty"})
+        if(Object.keys(rest).length>0) return res.status(400).send({statuis:false,message:"please provide valid keys"})
+        if(!longUrl) return res.status(400).send({statuis:false,message:"longUrl key must be present"})
+        if (isValid(longUrl)) return res.status(400).send({ status: false, message: "This URL is invalid" })
         
+    // if (!(/(ftp|http|https|FTP|HTTP|HTTPS):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(longUrl.trim())))
+      
+    //   return res
+    //     .status(400)
+    //     .send({ status: false, msg: "Enter a valid amazon S3 logo URL" });
+
+        let code = shortid.generate()
+        let details = {}
+        details.urlCode = code
+        details.longUrl = longUrl
+        details.shortUrl = `http://localhost:3000/${code}`
+        const createNewUrl = await urlModel.create(details)
+        let selectUrl = await urlModel.findOne(details).select({ urlCode: 1, longUrl: 1, shortUrl: 1, _id: 0 })
+        res.status(400).send({ status: true, data: selectUrl })
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message })
+
     }
 }
 
 
-module.exports={urlShortner}
+module.exports = { urlShortner }
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,6 +11,8 @@ router.get('/test', function (req, res){
 router.post('/url/shorten', urlShortner)
 
 
-
+router.all('/**',function(req,res){
+    res.status(404).send({status:false,message:'the api you request, Is not found'})
+})
 
 module.exports=router;
